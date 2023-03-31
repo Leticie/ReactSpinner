50f34af96bd2c2297d5b6697340cafcddc634f36
Leti <leticieouthierova@seznam.cz>
2023-03-31 15:11:34 +0200

    spin buttom working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [move, setMove] = useState("stop")
+  console.log(move)
+
+  const handleMove = () => {
+    if (move === "stop") {
+      setMove("spin")
+    }
+    else {
+      setMove("stop")
+    }  
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className="spin-button">SPIN</button>
+        <img src={logo} alt="logo" className={`App-logo-${move}`}/>
+        <button className="spin-button" onClick={handleMove}>SPIN</button>
       </header>
     </div>
   );
